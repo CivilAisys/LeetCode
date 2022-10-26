4d13c29bfffd41fee65603b06611d11ee7d3cfb3
CivilAisys <xx841995@gmail.com>
2022-10-26 22:11:25 +0800

    13

--- 13_RomanToInteger.js
+++ 13_RomanToInteger.js
@@ -2,20 +2,23 @@
  * @param {string} s
  * @return {number}
  */
-const romanToInt = function(s) {
+//解法1  1.先將字串IV.IX等特殊的解析並替換 2.解析替換後的字串作加總
+const romanToInt = function (s) {
     //羅馬字母對應數值
     const map = {
-        I:1,V:5,X:10,L:50,C:100,D:500,M:1000,IV:4,IX:9,XL:40,XL:90,CD:400,CM:900
+        I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
     }
     //要回傳的結果
     let result = 0;
-    
-    //解析字串並作加總
+
+    //將字串解析並替換
+    s = s.replace('IV', 'IIII').replace('IX', 'VIIII');
+    s = s.replace('XL', 'XXXX').replace('XC', 'LXXXX');
+    s = s.replace('CD', 'CCCC').replace('CM', 'DCCCC');
+
+    //遍歷替換後的字串
     for(let i = 0; i< s.length; i++){
-        //
-
-
-        result += map[s[i]]; 
+        result += map[s[i]];
     }
     return result;
 };
