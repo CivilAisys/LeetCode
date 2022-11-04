9d4cb17425c5d2c95bb31accddfb7a9236f1a5fd
Tony.Chen <tony.chen@hy-tech.com.tw>
2022-11-04 16:42:46 +0800

    24

--- 24_SwapNodesinPairs.js
+++ 24_SwapNodesinPairs.js
@@ -15,15 +15,25 @@ const swapPairs = function(head) {
     //若是head === null or head.next === null  就可直接回傳;
     if(head === null || head.next === null) return head;
 
-    //用number紀錄當前loop到第幾個節點
-    let number = 1;
-    //紀錄head是哪一個node
-    let header = head;
-    //紀錄當前loop到的節點  起點為head
-    let currentNode = head;
-    //紀錄loop時的前一個節點
-    let previousNode = null;
+    let dummy = new ListNode();
+    dummy.next = head;
+    let result = dummy;
 
+    //檢查下一個及下下一個是否有值(起始點前插入一個空的node)  有值才調換  調換後移動當前頭的位置再進行檢查及調換
+    while (dummy.next && dummy.next.next) {
+        //將p設置為1  q為2
+        let p = dummy.next, q = dummy.next.next;
+        //將1 設置為2
+        dummy.next = q;
+        //將1的下一個設置為3
+        p.next = q.next;    
+        //將2的下一個設置為1
+        q.next = p; 
+        //將節點移動至1
+        dummy = p
+    }
+
+    return result.next;
 };
 
 function ListNode(val, next) {
