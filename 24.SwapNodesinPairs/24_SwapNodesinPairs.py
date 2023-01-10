d77da69e8fc67b0e77b3507c3a65fd3841615a41
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-01-10 10:34:03 +0800

    新增24 python

--- 24.SwapNodesinPairs/24_SwapNodesinPairs.py
+++ 24.SwapNodesinPairs/24_SwapNodesinPairs.py
@@ -8,15 +8,27 @@ def swapPairs(head: ListNode) -> ListNode:
     if head == None or head.next == None:
         return head
     
-    #最後回傳result.next
-    result = ListNode()
-    result.next = head
-    
-    #需要檢查當前及下一個皆有值時才進行調換
-    while head != None and head.next != None:
+    #將head前套上node
+    fakeNode = ListNode()
+    fakeNode.next = head
+    #保存fakeNode地址
+    result = fakeNode
 
+    #需要檢查下個及下下個皆有值時才進行調換
+    while fakeNode.next != None and fakeNode.next.next != None:
+        #先保存下個及下下個的位址
+        a = fakeNode.next
+        b = fakeNode.next.next
 
-    
+        #改變fakeNode.next為b 0 -> 2
+        fakeNode.next = b
+        #改變 下個.next = b.next 0 -> 2 -> 1 -> 3
+        a.next = b.next
+        #改變下下個.next = a  0 -> 2 -> 1
+        b.next = a
+        #移動fakeNode指針至第二個 0 -> 2 -> 1(指針位置) -> 3
+        fakeNode = a
+
     return result.next
 
 
