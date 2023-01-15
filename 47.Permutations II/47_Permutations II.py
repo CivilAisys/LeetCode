bbd067a5d74b3b7a5d7f60de707b8bdacdd6e705
CivilAisys <xx841995@gmail.com>
2023-01-15 23:12:25 +0800

    新增 47 py

---
+++ 47.Permutations II/47_Permutations II.py
@@ -0,0 +1,23 @@
+# 解法1  使用backTracking進行窮舉  將所有可能組合列出  但組合不能夠重複
+def permuteUnique(nums: list[int]) -> list[list[int]]:
+    #結果集
+    result = []
+    permuteUniqueDFS(nums, len(nums), [], result)
+    return result
+
+#nums為當前決策可以選擇的數值 numsLength為最外層初始nums的長度  path為已決策的陣列 滿足條件為result
+def permuteUniqueDFS(nums: list[int], numsLength:int, path: list[int], result: list[list[int]]):
+    #決策中止條件
+    if numsLength == len(path):
+        result.append(path)
+
+    #將可決策數進行去重
+    new_List = list(set(nums))
+    #需要注意  為避免重複  當前決策只能選不相同的數字 故使用去重後陣列new_List
+    for index in range(len(new_List)):
+        #下回可決策數列 為nums - nums[index]
+        numsCopy = nums.copy()
+        numsCopy.remove(new_List[index])
+        permuteUniqueDFS(numsCopy, numsLength, path + [new_List[index]], result)
+
+permuteUnique([1,1,2])
