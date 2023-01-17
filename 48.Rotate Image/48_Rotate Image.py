1f1bd920a55d4c677a3ba7fbf17819c623c5ae23
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-01-17 17:59:48 +0800

    48 尚未完成

--- 48.Rotate Image/48_Rotate Image.py
+++ 48.Rotate Image/48_Rotate Image.py
@@ -6,7 +6,13 @@ def rotate(matrix: list[list[int]]) -> None:
     #上下轉置
     matrix.reverse()
 
-    #對矩陣進行
+    #對矩陣進行對稱互換
+
+    for i in range(len(matrix) - 1):
+        for j in range(len(matrix[i])):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    print(matrix)
 
 
-
+rotate([[1,2,3],[4,5,6],[7,8,9]])
