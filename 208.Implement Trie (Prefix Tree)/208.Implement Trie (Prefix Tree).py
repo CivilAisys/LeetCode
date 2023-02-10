688acd6b2b4b01fc1d6ccf3461adaf1e84548e8e
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-02-10 18:59:04 +0800

    208 py

--- 208.Implement Trie (Prefix Tree)/208.Implement Trie (Prefix Tree).py
+++ 208.Implement Trie (Prefix Tree)/208.Implement Trie (Prefix Tree).py
@@ -6,17 +6,30 @@
 class Trie:
 
     def __init__(self):
-        self.dic = {}
+        self.trie = {}
 
     def insert(self, word: str) -> None:
-        
-
+        t = self.trie
+        for w in word:
+            t = t.setdefault(w,{})
+        t['$'] = None
     def search(self, word: str) -> bool:
-        
+        t = self.trie
+        for w in word:
+            if w not in t:
+                return False
+            t = t[w]
+        if '$' in t:
+            return True
+        return False
 
     def startsWith(self, prefix: str) -> bool:
-        
-
+        t = self.trie
+        for w in prefix:
+            if w not in t:
+                return False
+            t = t[w]
+        return True
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
