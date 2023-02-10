cc0ca57552752520ba9b3ed5d8ab08ba8e010620
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-02-10 16:26:20 +0800

    208尚未完成

---
+++ 208.Implement Trie (Prefix Tree)/208.Implement Trie (Prefix Tree).py
@@ -0,0 +1,25 @@
+
+#解法1 
+#對每個結點開一個字母集大小的陣列，
+#對應的下標是兒子所表示的字母，內容則是這個兒子對應在大數位上的位置，即標號
+
+class Trie:
+
+    def __init__(self):
+        self.dic = {}
+
+    def insert(self, word: str) -> None:
+        
+
+    def search(self, word: str) -> bool:
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
