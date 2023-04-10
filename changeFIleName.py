d853b7fa95a8ef57017e64efc63f896c743c1ace
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-04-10 10:03:01 +0800

    變更資料夾名稱

---
+++ changeFIleName.py
@@ -0,0 +1,21 @@
+import os
+
+# 獲取當前資料夾路徑
+current_folder_path = os.getcwd()
+
+# 使用 os.listdir() 函式取得當前資料夾內所有的項目（包括檔案和資料夾）
+all_items = os.listdir(current_folder_path)
+
+# 過濾出只是資料夾的項目
+folders = [item for item in all_items if os.path.isdir(
+    os.path.join(current_folder_path, item))]
+
+# 列印出所有資料夾的名稱
+print("當前資料夾內的資料夾：")
+for folder in folders:
+    if folder.startswith("."):
+        continue
+    else:
+        os.rename(folder, folder[1:])
+        
+
