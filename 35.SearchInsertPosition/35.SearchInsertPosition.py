6a8a891b0b7e84b476dfdb74ddf70fa60701d019
CivilAisys <xx841995@gmail.com>
2023-07-23 19:44:28 +0800

    調整35取mid方法

--- 35.SearchInsertPosition/35.SearchInsertPosition.py
+++ 35.SearchInsertPosition/35.SearchInsertPosition.py
@@ -4,7 +4,7 @@ def searchInsert(nums: list[int], target: int) -> int:
     
     #找尋第一個大於target的索引  回傳索引 - 1
     while left <= right:
-        mid = (left + right) // 2
+        mid = left + (right - left + 1) // 2
 
         if nums[mid] > target:
             right = mid - 1
