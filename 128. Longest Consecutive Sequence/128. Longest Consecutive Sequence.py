72c130182fc9c0df34ad96d0cafd68a56054d5c2
Tony.Chen <tony.chen@hy-tech.com.tw>
2023-08-05 22:12:55 +0800

    修正128

--- 128. Longest Consecutive Sequence/128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence/128. Longest Consecutive Sequence.py
@@ -7,7 +7,7 @@ class Solution:
         for num in num_set:
             if num - 1 not in num_set: # 確認是否為序列起頭
                 current_num = num
-                current_streak = 11
+                current_streak = 1
                 while current_num + 1 in num_set:
                     current_num += 1
                     current_streak += 1
